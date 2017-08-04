793a1e1bd23d303d37491891e94adc7c16a88361
Sehriban Cheng <sehriban@Sehribans-MacBook.local>
2017-08-03 18:39:07 -0700

    creted gane logic

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 console.log("this is the test");
 console.log( "hello world");
+
+var cardOne = "queen";
+
+
+
+var cardTwo = "queen";
+
+
+var cardThree = "king";
+
+
+
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
