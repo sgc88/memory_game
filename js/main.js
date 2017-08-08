ea718daa2a230b68407022feb9b6539b027645ef
Sehriban Cheng <sehriban@Sehribans-MacBook.local>
2017-08-07 21:18:05 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-console.log("this is the test");
-console.log( "hello world");
+var cards = ["queen", "queen", "king", "king"];
 
-var cardOne = "queen";
+var cardsInPlay = [];
 
+var cardOne = cards[0];
 
+cardsInPlay.push(cardOne);
 
-var cardTwo = "queen";
+console.log("user flipped " + cardsInPlay[0]);
 
+var cardTwo = cards[2];
 
-var cardThree = "king";
+cardsInPlay.push(cardTwo);
 
+console.log("user flipped " + cardsInPlay[1]);
 
+if(cardsInPlay.length === 2){
 
-var cardFour = "king";
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match!");
+	}else{
+		alert("sorry, try again");
+	}
+}
