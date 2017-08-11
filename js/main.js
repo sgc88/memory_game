5f0bddb62698478516808e66fb3a65ed96927dcb
Sehriban Cheng <sehriban@Sehribans-MacBook.local>
2017-08-11 01:54:58 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
 
-var cards = ["queen", "queen", "king", "king"];
+
+
+
+var cards = [
+
+	{
+		rank : "queen",
+		suit : "hearts",
+		cardImage: "images/queen-of-hearts.png",
+
+	},
+
+	{
+		rank : "queen",
+		suit :  "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+
+	},
+	{
+		rank : "king",
+		suit :"hearts",
+		cardImage:"images/king-of-hearts.png",
+
+	},
+
+	{
+		rank : "king",
+		suit :"diamonds",
+		cardImage:"images/king-of-diamonds.png",
+
+	},
+
+];
 
 var cardsInPlay = [];
 
@@ -9,15 +41,17 @@ function checkForMatch(){
 	}else{
 		alert("sorry, try again");
 	}
-}
+};
 
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank );
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if(cardsInPlay.length === 2){
 		checkForMatch();
 	}
-}
+};
 
 flipCard(0);
 flipCard(2);
